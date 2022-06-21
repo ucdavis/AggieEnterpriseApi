1ce7cf3e91db364d73463f04d950ebeb905b4f8b
Scott Kirkland <srkirkland@gmail.com>
2022-06-21 10:44:42 -0700

    note to self

--- src/AggieEnterpriseApi/GraphQlClient.cs
+++ src/AggieEnterpriseApi/GraphQlClient.cs
@@ -9,6 +9,8 @@ public class GraphQlClient
         var graphQlUri = new Uri(url);
 
         var serviceCollection = new ServiceCollection();
+        
+        // TODO: likely need serializers for custom int/float types (ex: PositiveInt)
 
         serviceCollection
             .AddAggieEnterpriseClient()
