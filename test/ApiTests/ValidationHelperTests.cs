de45280c5132a004236be730204840750f2cbe4e
Scott Kirkland <srkirkland@gmail.com>
2022-07-12 10:17:13 -0700

    test those tests

---
+++ test/ApiTests/ValidationHelperTests.cs
@@ -0,0 +1,61 @@
+using System.Threading.Tasks;
+using AggieEnterpriseApi;
+using AggieEnterpriseApi.Extensions;
+using AggieEnterpriseApi.Validation;
+using ApiTests.Setup;
+using Xunit;
+using Shouldly;
+using Microsoft.Extensions.Configuration;
+
+
+namespace ApiTests;
+public class ValidationHelperTests : TestBase
+{
+    [Fact]
+    public void ValidGlString()
+    {
+        var glString = "3110-72160-9300202-775000-85-000-0000000000-000000-0000-000000-000000";
+        
+        FinancialChartValidation.IsValidGlSegmentString(glString).ShouldBeTrue();
+        FinancialChartValidation.GetFinancialChartStringType(glString).ShouldBe(FinancialChartStringType.Gl);
+    }
+    
+    [Fact]
+    public void ValidGlSegments()
+    {
+        var glString = "3110-72160-9300202-775000-85-000-0000000000-000000-0000-000000-000000";
+
+        var segments = FinancialChartValidation.GetGlSegments(glString);
+        
+        segments.Entity.ShouldBe("3110");
+        // TODO: validate rest of segments
+    }
+    
+    [Fact]
+    public void ValidPpmStringRequiredOnly()
+    {
+        var ppmString = "SP00000001-000001-0000000-000000";
+        
+        FinancialChartValidation.IsValidPpmSegmentString(ppmString).ShouldBeTrue();
+        FinancialChartValidation.GetFinancialChartStringType(ppmString).ShouldBe(FinancialChartStringType.Ppm);
+    }
+    
+    [Fact]
+    public void ValidPpmStringFull()
+    {
+        var ppmString = "CP00000001-000001-0000000-000000-0000000-00000";
+        
+        FinancialChartValidation.IsValidPpmSegmentString(ppmString).ShouldBeTrue();
+        FinancialChartValidation.GetFinancialChartStringType(ppmString).ShouldBe(FinancialChartStringType.Ppm);
+    }
+    
+    [Fact]
+    public void ValidPpmSegments()
+    {
+        var ppmString = "CP00000001-000001-0000000-000000-0000000-00000";
+
+        var segments = FinancialChartValidation.GetPpmSegments(ppmString);
+        
+        segments.Project.ShouldBe("CP00000001");
+    }
+}
