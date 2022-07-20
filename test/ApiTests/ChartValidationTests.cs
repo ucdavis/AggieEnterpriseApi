1b4550a8c39ac40b2714c629f8dc03a525a87cac
Jason Sylvestre <jsylvestre@ucdavis.edu>
2022-07-20 08:43:45 -0700

    Use a valid COA

--- test/ApiTests/ChartValidationTests.cs
+++ test/ApiTests/ChartValidationTests.cs
@@ -15,15 +15,15 @@ public class ChartValidationTests : TestBase
     {
         var client = AggieEnterpriseApi.GraphQlClient.Get(GraphQlUrl, Token);
 
-        var result = await client.GlValidateChartstring.ExecuteAsync("3110-72160-9300202-775000-85-000-0000000000-000000-0000-000000-000000", false);
+        var result = await client.GlValidateChartstring.ExecuteAsync("3110-13U20-ADNO003-410000-43-000-0000000000-000000-0000-000000-000000", false);
 
         var data = result.ReadData();
         data.GlValidateChartstring.ValidationResponse.Valid.ShouldBeTrue("Response should be valid");
         data.GlValidateChartstring.Segments.Entity.ShouldBe("3110");
-        data.GlValidateChartstring.Segments.Fund.ShouldBe("72160");
-        data.GlValidateChartstring.Segments.Department.ShouldBe("9300202");
-        data.GlValidateChartstring.Segments.Account.ShouldBe("775000");
-        data.GlValidateChartstring.Segments.Purpose.ShouldBe("85");
+        data.GlValidateChartstring.Segments.Fund.ShouldBe("13U20");
+        data.GlValidateChartstring.Segments.Department.ShouldBe("ADNO003");
+        data.GlValidateChartstring.Segments.Account.ShouldBe("410000");
+        data.GlValidateChartstring.Segments.Purpose.ShouldBe("43");
         data.GlValidateChartstring.Segments.Project.ShouldBe("0000000000");
         data.GlValidateChartstring.Segments.Program.ShouldBe("000");
         data.GlValidateChartstring.Segments.Activity.ShouldBe("000000");
@@ -37,10 +37,10 @@ public class ChartValidationTests : TestBase
         var segments = new GlSegmentInput
         {
             Entity = "3110",
-            Fund = "72160",
-            Department = "9300202",
-            Account = "775000",
-            Purpose = "85",
+            Fund = "13U20",
+            Department = "ADNO003",
+            Account = "410000",
+            Purpose = "43",
             Project = "0000000000",
             Program = "000",
             Activity = "000000",
@@ -51,7 +51,7 @@ public class ChartValidationTests : TestBase
 
         var data = result.ReadData();
         data.GlValidateChartSegments.ValidationResponse.Valid.ShouldBeTrue("Response should be valid");
-        data.GlValidateChartSegments.CompleteChartstring.ShouldBe("3110-72160-9300202-775000-85-000-0000000000-000000-0000-000000-000000");
+        data.GlValidateChartSegments.CompleteChartstring.ShouldBe("3110-13U20-ADNO003-410000-43-000-0000000000-000000-0000-000000-000000");
     }
 
     [Fact]
@@ -62,19 +62,19 @@ public class ChartValidationTests : TestBase
         var segments = new GlSegmentInput
         {
             Entity = "3110",
-            Fund = "72160",
-            Department = "9300202",
-            Account = "775000",
-            Purpose = "85",
+            Fund = "13U20",
+            Department = "ADNO003",
+            Account = "410000",
+            Purpose = "43",
             Project = "0000000000",
             Program = "000",
-            Activity = "000000"
+            Activity = "000000",
         };
         var result = await client.GlValidateChartSegments.ExecuteAsync(segments, false);
 
         var data = result.ReadData();
         data.GlValidateChartSegments.ValidationResponse.Valid.ShouldBeTrue("Response should be valid");
-        data.GlValidateChartSegments.CompleteChartstring.ShouldBe("3110-72160-9300202-775000-85-000-0000000000-000000-0000-000000-000000");
+        data.GlValidateChartSegments.CompleteChartstring.ShouldBe("3110-13U20-ADNO003-410000-43-000-0000000000-000000-0000-000000-000000");
     }
 
     [Fact]
