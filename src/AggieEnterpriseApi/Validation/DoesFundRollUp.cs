f37c302971ed2dada215546e27e57df9cffdf98e
Jason Sylvestre <jsylvestre@ucdavis.edu>
2022-08-05 11:16:50 -0700

    Add check in the API

---
+++ src/AggieEnterpriseApi/Validation/DoesFundRollUp.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AggieEnterpriseApi.Validation
+{
+    public static class DoesFundRollUp
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="fund"></param>
+        /// <param name="level">HierarchyDepth</param>
+        /// <param name="expectedCode"></param>
+        /// <returns></returns>
+        public static bool Fund(IFundParents_ErpFund? fund, int level, string expectedCode)
+        {
+            if (fund == null)
+            {
+                return false;
+            }
+            if (fund.HierarchyDepth == level && fund.Code == expectedCode)
+            {
+                return true;
+            }
+            if (fund.Parent == null)
+            {
+                return false;
+            }
+
+            if (fund.Parent.HierarchyDepth == level && fund.Parent.Code == expectedCode)
+            {
+                return true;
+            }
+            if (fund.Parent.Parent == null)
+            {
+                return false;
+            }
+
+            if (fund.Parent.Parent.HierarchyDepth == level && fund.Parent.Parent.Code == expectedCode)
+            {
+                return true;
+            }
+            if (fund.Parent.Parent.Parent == null)
+            {
+                return false;
+            }
+
+            if (fund.Parent.Parent.Parent.HierarchyDepth == level && fund.Parent.Parent.Parent.Code == expectedCode)
+            {
+                return true;
+            }
+            if (fund.Parent.Parent.Parent.Parent == null)
+            {
+                return false;
+            }
+
+            if (fund.Parent.Parent.Parent.Parent.HierarchyDepth == level && fund.Parent.Parent.Parent.Parent.Code == expectedCode)
+            {
+                return true;
+            }
+            if (fund.Parent.Parent.Parent.Parent.Parent == null)
+            {
+                return false;
+            }
+            if (fund.Parent.Parent.Parent.Parent.Parent.HierarchyDepth == level && fund.Parent.Parent.Parent.Parent.Code == expectedCode)
+            {
+                return true;
+            }
+
+            if (fund.Parent.Parent.Parent.Parent.Parent.Parent == null)
+            {
+                return false;
+            }
+            if (fund.Parent.Parent.Parent.Parent.Parent.Parent.HierarchyDepth == level && fund.Parent.Parent.Parent.Parent.Code == expectedCode)
+            {
+                return true;
+            }
+
+            if (fund.Parent.Parent.Parent.Parent.Parent.Parent.Parent == null)
+            {
+                return false;
+            }
+            if (fund.Parent.Parent.Parent.Parent.Parent.Parent.Parent.HierarchyDepth == level && fund.Parent.Parent.Parent.Parent.Code == expectedCode)
+            {
+                return true;
+            }
+
+
+            return false;
+        }
+    }
+}
--- test/ApiTests/FundTests.cs
+++ test/ApiTests/FundTests.cs
@@ -2,6 +2,7 @@ using System;
 using System.Threading.Tasks;
 using AggieEnterpriseApi;
 using AggieEnterpriseApi.Extensions;
+using AggieEnterpriseApi.Validation;
 using ApiTests.Setup;
 using Shouldly;
 using Xunit;
@@ -26,63 +27,10 @@ public class FundTests : TestBase
         data.ErpFund.Parent.Parent.ShouldNotBeNull();
         data.ErpFund.Parent.Parent.Code.ShouldBe("1200C");
 
-        DoesItRollUp(data.ErpFund, 2, "1200C").ShouldBeTrue();
-        DoesItRollUp(data.ErpFund, 1, "1200C").ShouldBeFalse();
+
+        DoesFundRollUp.Fund(data.ErpFund, 2, "1200C").ShouldBeTrue();
+        DoesFundRollUp.Fund(data.ErpFund, 1, "1200C").ShouldBeFalse();
     }
 
-    private bool DoesItRollUp(IFundParents_ErpFund? fund, int level, string expectedCode)
-    {
-        if (fund == null)
-        {
-            return false;
-        }
-        if (fund.HierarchyDepth == level && fund.Code == expectedCode)
-        {
-            return true;
-        }
-        if (fund.Parent == null)
-        {
-            return false;
-        }
-        
-        if (fund.Parent.HierarchyDepth == level && fund.Parent.Code == expectedCode)
-        {
-            return true;
-        }
-        if (fund.Parent.Parent == null)
-        {
-            return false;
-        }
-
-        if (fund.Parent.Parent.HierarchyDepth == level && fund.Parent.Parent.Code == expectedCode)
-        {
-            return true;
-        }
-        if (fund.Parent.Parent.Parent == null)
-        {
-            return false;
-        }
-
-        if (fund.Parent.Parent.Parent.HierarchyDepth == level && fund.Parent.Parent.Parent.Code == expectedCode)
-        {
-            return true;
-        }
-        if (fund.Parent.Parent.Parent.Parent == null)
-        {
-            return false;
-        }
-
-        if (fund.Parent.Parent.Parent.Parent.HierarchyDepth == level && fund.Parent.Parent.Parent.Parent.Code == expectedCode)
-        {
-            return true;
-        }
-        if (fund.Parent.Parent.Parent.Parent.Parent == null)
-        {
-            return false;
-        }
-
-
-        return false;
-    }
 
 }
