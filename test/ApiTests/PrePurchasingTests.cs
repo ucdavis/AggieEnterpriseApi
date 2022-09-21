d427d1b75bfc974216f91aff58153014bbfb10ac
Jason Sylvestre <jsylvestre@ucdavis.edu>
2022-09-21 13:56:09 -0700

    Failure where distributions do not add up

--- test/ApiTests/PrePurchasingTests.cs
+++ test/ApiTests/PrePurchasingTests.cs
@@ -173,4 +173,123 @@ public class PrePurchasingTests : TestBase
     }
 
 
+    [Fact]
+    public async Task CreateInvalidRequsition()
+    {
+        var client = AggieEnterpriseApi.GraphQlClient.Get(GraphQlUrl, Token);
+
+        var saveId = Guid.NewGuid().ToString();
+
+        var inputOrder = new ScmPurchaseRequisitionRequestInput
+        {
+            Header = new ActionRequestHeaderInput
+            {
+                ConsumerTrackingId = saveId,
+                ConsumerReferenceId = "ACRU-EHIT218 - TEST",
+                ConsumerNotes = "Workgroup: CRU Internal Workgroup",
+                BoundaryApplicationName = "PrePurchasing",
+                ConsumerId = "UCD Giving Service", //Remove after fixed 9/23/2022
+            }
+        };
+
+        var search = new ScmSupplierFilterInput { SupplierNumber = new StringFilterInput { Eq = "83526" } };
+        var searchResult = await client.ScmSupplierSearch.ExecuteAsync(search);
+        var searchData = searchResult.ReadData();
+
+        inputOrder.Payload = new ScmPurchaseRequisitionInput
+        {
+            RequisitionSourceName = "UCD SLOTH",
+            SupplierNumber = searchData.ScmSupplierSearch.Data.First().SupplierNumber.ToString(),
+            SupplierSiteCode = searchData.ScmSupplierSearch.Data.First().Sites.Where(a => a.Location?.City == "ROUND ROCK" && a.Location?.AddressLine2 == "ONE DELL WAY" && a.Location?.State == "TX").First().SupplierSiteCode,
+            RequesterEmailAddress = "jsylvestre@ucdavis.edu",
+            Description = "ACRU-EHIT218 - TEST",
+            Justification = "Print Toner for PrintsCharming 38 Mrak Dell eQuote #3000118141264",
+        };
+
+        var distributionResult = await client.KfsConvertAccount.ExecuteAsync("3", "CRU9033", null);
+        var distributionData = distributionResult.ReadData();
+        var segmentString = "";
+        if (distributionData.KfsConvertAccount.GlSegments != null)
+        {
+            segmentString = new GlSegments(distributionData.KfsConvertAccount.GlSegments).ToSegmentString();
+        }
+
+        var Line1 = new ScmPurchaseRequisitionLineInput
+        {
+            Amount = 102.99m,
+            Quantity = 1,
+            UnitOfMeasure = "Each",
+            ItemDescription = "Dell 2155cnd Black Toner",
+            PurchasingCategoryName = "15000FAC", //Completely faked
+            UnitPrice = 102.99m,
+            Distributions = new[]
+            {
+                new ScmPurchaseRequisitionDistributionInput
+                {
+                    Amount = 302.99m, //Too big
+                    GlSegmentString = segmentString,
+                }
+            },
+        };
+
+        var Line2 = new ScmPurchaseRequisitionLineInput
+        {
+            Amount = 83.99m,
+            Quantity = 1,
+            UnitOfMeasure = "Each",
+            ItemDescription = "Dell 2155cdn Cyan Toner",
+            PurchasingCategoryName = "15000FAC", //Completely faked
+            UnitPrice = 83.99m,
+            Distributions = new[]
+            {
+                new ScmPurchaseRequisitionDistributionInput
+                {
+                    Amount = 83.99m,
+                    GlSegmentString = segmentString,
+                }
+            },
+        };
+
+        var Line3 = new ScmPurchaseRequisitionLineInput
+        {
+            Amount = 101.99m,
+            Quantity = 1,
+            UnitOfMeasure = "Each",
+            ItemDescription = "Dell 2155cdn Magenta Toner",
+            PurchasingCategoryName = "15000FAC", //Completely faked
+            UnitPrice = 101.99m,
+            Distributions = new[]
+            {
+                new ScmPurchaseRequisitionDistributionInput
+                {
+                    Amount = 101.99m,
+                    GlSegmentString = segmentString,
+                }
+            },
+        };
+
+        var Lines = new List<ScmPurchaseRequisitionLineInput>(3);
+        Lines.Add(Line1);
+        Lines.Add(Line2);
+        Lines.Add(Line3);
+
+        inputOrder.Payload.Lines = Lines;
+
+
+
+        var NewOrderRequsition = await client.ScmPurchaseRequisitionCreate.ExecuteAsync(inputOrder);
+
+        var data = NewOrderRequsition.ReadData();
+
+        data.ScmPurchaseRequisitionCreate.ShouldNotBeNull();
+        data.ScmPurchaseRequisitionCreate.RequestStatus.ShouldNotBeNull();
+        data.ScmPurchaseRequisitionCreate.RequestStatus.RequestId.ShouldNotBeNull();
+        data.ScmPurchaseRequisitionCreate.RequestStatus.RequestStatus.ShouldBe(RequestStatus.Rejected);
+        data.ScmPurchaseRequisitionCreate.ValidationResults.ShouldNotBeNull();
+        data.ScmPurchaseRequisitionCreate.ValidationResults.Valid.ShouldBeFalse();
+        data.ScmPurchaseRequisitionCreate.ValidationResults.ErrorMessages.ShouldNotBeNull();
+        data.ScmPurchaseRequisitionCreate.ValidationResults.ErrorMessages.Count.ShouldBe(1);
+        data.ScmPurchaseRequisitionCreate.ValidationResults.ErrorMessages.First().ShouldBe("Line amount (102.99) is not sum of distributions (302.99).");
+
+    }
 }
