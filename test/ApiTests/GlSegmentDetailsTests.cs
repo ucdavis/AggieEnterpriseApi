1f245ef392f51c33eba605d1085490814840813e
Jason Sylvestre <jsylvestre@ucdavis.edu>
2023-05-26 09:44:35 -0700

    Test

--- test/ApiTests/GlSegmentDetailsTests.cs
+++ test/ApiTests/GlSegmentDetailsTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 using AggieEnterpriseApi;
 using AggieEnterpriseApi.Extensions;
@@ -14,7 +15,7 @@ public class GlSegmentDetailsTests : TestBase
     [Fact]
     public async Task GetDetails()
     {
-        var client = AggieEnterpriseApi.GraphQlClient.Get(GraphQlUrl, Token);
+        var client = AggieEnterpriseApi.GraphQlClient.Get(GraphQlUrl, TokenEndpoint, ConsumerKey, ConsumerSecret, $"{ScopeApp}-{ScopeEnv}");
 
         var result = await client.CoaDetails.ExecuteAsync(entity: "3110", fund: "12100", dept: "ADNO003", account: "775000", purpose:"45");
         
@@ -24,11 +25,11 @@ public class GlSegmentDetailsTests : TestBase
 
         data.ErpEntity.ShouldNotBeNull();
         data.ErpEntity.Code.ShouldBe("3110");
-        data.ErpEntity.Name.ShouldBe("UC Davis Campus Excluding School of Health");
+        data.ErpEntity.Name.ShouldBe("UC Davis Campus");
 
         data.ErpFund.ShouldNotBeNull();        
         data.ErpFund.Code.ShouldBe("12100");
-        data.ErpFund.Name.ShouldBe("SalesandServices Rate Based");
+        data.ErpFund.Name.ShouldBe("Sales and Services Rate Based");
 
         data.ErpFinancialDepartment.ShouldNotBeNull();
         data.ErpFinancialDepartment.Code.ShouldBe("ADNO003");
@@ -47,10 +48,25 @@ public class GlSegmentDetailsTests : TestBase
     [Fact]
     public async Task GetDepartmentApprovers()
     {
-        var client = AggieEnterpriseApi.GraphQlClient.Get(GraphQlUrl, Token);
+        var client = AggieEnterpriseApi.GraphQlClient.Get(GraphQlUrl, TokenEndpoint, ConsumerKey, ConsumerSecret, $"{ScopeApp}-{ScopeEnv}");
 
-        //var result = await client.erpd
+        var result = await client.ErpDepartmentApprovers.ExecuteAsync("ADNO003");
+
+        var data = result.ReadData();
+        data.ShouldNotBeNull();
+        data.ErpFinancialDepartment.ShouldNotBeNull();
+        data.ErpFinancialDepartment.Code.ShouldBe("ADNO003");
+        data.ErpFinancialDepartment.Approvers.ShouldNotBeNull();
+        data.ErpFinancialDepartment.Approvers.Count.ShouldBe(6);
+        var FoApprovers = data.ErpFinancialDepartment.Approvers.Where(a => a.ApproverType == "Fiscal Officer Approver").ToList();
+        FoApprovers.Count().ShouldBe(2);
+        FoApprovers[0].EmailAddress.ShouldBe("bmceligot@ucdavis.edu");
+        FoApprovers[0].FirstName.ShouldBe("Brian");
+        FoApprovers[0].LastName.ShouldBe("Mceligot");
+        FoApprovers[0].UserId.ShouldBe("mceligot");
+        FoApprovers[1].EmailAddress.ShouldBe("satanguay@ucdavis.edu");
+        FoApprovers[1].FirstName.ShouldBe("Shannon");
+        FoApprovers[1].LastName.ShouldBe("Tanguay");
+        FoApprovers[1].UserId.ShouldBe("tanguay");
     }
-
-
 }
