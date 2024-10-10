18c8e46266d4a4ee9c6a73739871ef0d8361402f
Jason Sylvestre <jsylvestre@ucdavis.edu>
2024-10-10 10:15:29 -0700

    For Suppliers (Vendors) make sure they can be used

--- test/ApiTests/PrePurchasingTests.cs
+++ test/ApiTests/PrePurchasingTests.cs
@@ -314,7 +314,7 @@ public class PrePurchasingTests : TestBase
             ItemDescription = "Dell 2155cnd Black Toner",
             PurchasingCategoryName = "15000FAC", //Completely faked
             UnitPrice = 102.99m,
-            SupplierItem = "331-0716", //Faked, new field in UAT
+            //SupplierItem = "331-0716", //Faked, new field in UAT //TODO: Test when this is in prod
             Distributions = new[]
             {
                 new ScmPurchaseRequisitionDistributionInput
